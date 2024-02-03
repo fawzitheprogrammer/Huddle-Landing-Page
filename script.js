a5ad178b42270498658407953cf8f29bc87f67f5
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-04 01:16:02 +0300

    Final Commit

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-
 function animateNumber(elementId, startValue, endValue,unit, duration) {
-    const element = document.getElementById(elementId);
-    if (!element) {
-      console.error(`Element with ID ${elementId} not found.`);
-      return;
-    }
-  
-    let currentVal = startValue;
-    const stepTime = duration / (endValue - startValue);
-  
-    const interval = setInterval(() => {
-      currentVal++;
-      element.textContent = currentVal > 999 ? (currentVal / 1000).toFixed(1) + unit : currentVal;
-  
-      if (currentVal >= endValue) clearInterval(interval);
-    }, stepTime);
+  const element = document.getElementById(elementId);
+  if (!element) {
+    console.error(`Element with ID ${elementId} not found.`);
+    return;
   }
 
-  document.addEventListener('DOMContentLoaded', function() {
-    animateNumber('communities', 500, 1200,'k', 5000);
-    animateNumber('message', 800, 2700,'m', 5000);
-    
-  });
+  let currentVal = startValue;
+  const stepTime = duration / (endValue - startValue);
+
+  const interval = setInterval(() => {
+    currentVal++;
+    element.textContent = currentVal > 999 ? (currentVal / 1000).toFixed(1) + unit : currentVal;
+
+    if (currentVal >= endValue) clearInterval(interval);
+  }, stepTime);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  animateNumber('communities', 100, 1200,'k', 5000);
+  animateNumber('message', 100, 2700,'m', 5000);
+  
+});
